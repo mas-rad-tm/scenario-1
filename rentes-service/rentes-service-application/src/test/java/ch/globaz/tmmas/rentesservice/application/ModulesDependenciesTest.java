6efda4ad76f1fc1a1ce2e54465064f23ca8f2d5f
Sébastien Chèvre <seb.chevre@gmail.com>
2018-07-18 07:28:50 +0200

    add junit5 dependencies

--- rentes-service/rentes-service-application/src/test/java/ch/globaz/tmmas/rentesservice/application/ModulesDependenciesTest.java
+++ rentes-service/rentes-service-application/src/test/java/ch/globaz/tmmas/rentesservice/application/ModulesDependenciesTest.java
@@ -3,9 +3,14 @@ package ch.globaz.tmmas.rentesservice.application;
 
 public class ModulesDependenciesTest {
 
-    /**
+/**
     private static JavaClasses classes;
 
+    @BeforeAll
+    static void setup() {
+        log.info("@BeforeAll - executes once before all test methods in this class");
+    }
+    /*
     @Before
     public static void setUpClasses() {
         classes = new ClassFileImporter().importPackages(BASE_PACKAGE_NAME);
