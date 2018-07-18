dcb91d9dff7b17d7bde037f4a08363f2e046d27c
SCE <sce@gglobaz.ch>
2018-07-18 07:30:41 +0200

    merge

--- rentes-service/rentes-service-application/src/test/java/ch/globaz/tmmas/rentesservice/application/ModulesDependenciesTest.java
+++ rentes-service/rentes-service-application/src/test/java/ch/globaz/tmmas/rentesservice/application/ModulesDependenciesTest.java
@@ -1,14 +1,18 @@
 package ch.globaz.tmmas.rentesservice.application;
 
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+
 public class ModulesDependenciesTest {
 
-    /**
+
     private static JavaClasses classes;
 
-    @Before
+
+    @BeforeAll
     public static void setUpClasses() {
         classes = new ClassFileImporter().importPackages(BASE_PACKAGE_NAME);
-    }*/
+    }
 
 }
